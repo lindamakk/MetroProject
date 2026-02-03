23478f7ec0243c3bfee3e59a3dec76323013472a
Lindamakk <lindamak2001@gmail.com>
2026-02-03 12:20:36 +0300

    feat: done StationCard

--- MetroProject/Components/StationCard.swift
+++ MetroProject/Components/StationCard.swift
@@ -5,3 +5,31 @@
 //  Created by Linda on 02/02/2026.
 //
 
+import SwiftUI
+
+struct StationCard: View {
+    
+    var lineColor: Color = Color.yellow
+    var stopName: String = "Princess Noura University 1"
+    var isSelected: Bool = false
+    var body: some View {
+
+        
+        
+        VStack(alignment: .leading) {
+            Text(stopName).font(Font.system(size: 14, weight: .medium, design: .default))
+        }.padding()
+            .frame(maxWidth: .infinity,  alignment: .leading)
+            .frame(height: 40)
+            .overlay(
+                   RoundedRectangle(cornerRadius: 10)
+                    .stroke(isSelected ? lineColor : Color.gray, lineWidth: isSelected ? 2 : 1)).padding()
+//
+    
+    }
+}
+
+
+#Preview {
+    StationCard()
+}
