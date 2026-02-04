8d4c34285914549db9450aab832c8100bc58fcdb
DanaNB02 <dbaeisa25@twq.idserve.net>
2026-02-04 11:11:43 +0300

    SearchBar component

--- MetroProject/Components/SearchBar.swift
+++ MetroProject/Components/SearchBar.swift
@@ -1,7 +1,25 @@
-//
-//  SearchBar.swift
-//  MetroProject
-//
-//  Created by Linda on 02/02/2026.
-//
+// Dana
+import SwiftUI
 
+struct SearchBar: View {
+    
+    @State private var searchText = ""
+    var body: some View {
+            NavigationStack {
+                List {
+                    // TODO: List of stations
+              
+                }
+                // must attack to a child view inside NavigationStack
+                .searchable(
+                    text: $searchText,
+                    placement: .navigationBarDrawer(displayMode: .always),
+                    prompt: "Search for a station.."
+                )
+            }
+        }
+}
+
+#Preview {
+    SearchBar()
+}
