48b40e53076e1568e0d3047f02edcb8a35d87336
DanaNB02 <dbaeisa25@twq.idserve.net>
2026-02-05 11:56:55 +0300

    Reach destination view

--- MetroProject/Views/TripEndedView.swift
+++ MetroProject/Views/TripEndedView.swift
@@ -1,15 +1,53 @@
-//
-//  TripEndedView.swift
-//  MetroProject
-//
-//  Created by Linda on 02/02/2026.
-//
+// Dana
 
 
 import SwiftUI
 
 struct TripEndedView: View {
     var body: some View {
-        Text("Trip Ended")
+     
+        VStack {
+            
+            // icon
+            ZStack {
+                Circle()
+                    .fill(Color("GreenPrimaryColor"))
+                    .frame(width: 104, height: 104)
+
+                Image(systemName: "checkmark.circle.dotted" )
+                    .foregroundStyle(Color("BackgroundColor"))
+                    .font(.system(size: 52, weight: .regular))
+                    .accessibilityLabel("Reach Destination")
+            }
+            
+            // text
+            Text("You’ve reached your destination successfully")
+                .font(.system(size: 24, weight: .regular))
+                .foregroundColor(.black)
+                .multilineTextAlignment(.center)
+                .padding(.top, 40)
+                .padding(.bottom, 5)
+                .padding(.horizontal, 50)
+            
+            Text("Have a great Day!")
+                .font(.system(size: 18, weight: .regular))
+                .foregroundColor(.gray)
+            
+            
+            // done button
+            ActionButton(label: "Done") {
+                print("Started")
+            }
+            .offset(x: 0, y: 205)
+            
+        }
+    
+        
+        
+      
     }
+}
+
+#Preview {
+    TripEndedView()
 }
