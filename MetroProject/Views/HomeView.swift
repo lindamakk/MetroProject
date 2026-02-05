4fe1ac961be5842a7ac41b6caf5988fcf5295181
Reemi <121254688+rareemi@users.noreply.github.com>
2026-02-05 12:14:16 +0300

    Home page added, edited: primary button, Start trip card and last trip card

--- MetroProject/Views/HomeView.swift
+++ MetroProject/Views/HomeView.swift
@@ -7,13 +7,38 @@
 
 import SwiftUI
 
-
 struct HomeView: View {
     var body: some View {
-        Text("Home View")
+        VStack(alignment: .leading, spacing: 24) {
+
+            Text("Plan Trip")
+                .font(.system(size: 28, weight: .semibold))
+                .foregroundColor(.black)
+                .padding(.top, 24)
+                .padding(.horizontal, 16)
+
+            // الكرت الأول بمحاذاة الوسط
+            HStack {
+                Spacer()
+                StartTripCard()
+                Spacer()
+            }
+
+            Text("Last Trips")
+                .font(.system(size: 16, weight: .regular))
+                .foregroundColor(.black)
+                .padding(.top, 8)
+                .padding(.horizontal, 16)
+
+            HStack {
+                LastTripCard(stationName: "King Saud University")
+                Spacer()
+            }.padding()
+
+            Spacer(minLength: 0)
+        }
     }
 }
-
 
 #Preview {
     HomeView()
