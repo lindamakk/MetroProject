bde34225c9d1c8b66871c68eadafac5fdee25b8f
Asaylhussin04 <asaylhussin77@gmail.com>
2026-02-08 12:43:33 +0300

    currentTripView

--- MetroProject/Views/CurrentTripView.swift
+++ MetroProject/Views/CurrentTripView.swift
@@ -1,15 +1,32 @@
-//
-//  CurrentTripView.swift
-//  MetroProject
-//
-//  Created by Linda on 02/02/2026.
-//
-
-
 import SwiftUI
-
 struct CurrentTripView: View {
+    
+    let currentUserDestination = userDestination(stationName: "King Saud University", lineName: "Red")
+    
     var body: some View {
-        Text("CurrentTripView")
+        VStack(spacing: 60) {
+            Spacer().frame(height: 40)
+            
+            FinalDestinationGroup(userDestination: currentUserDestination)
+            TripCardView(stopsLeft: 2, status: .inProgress)
+            
+            //TripCardView(stopsLeft: 1, status: .interrupted)
+            
+            Spacer()
+            
+            //end trip button
+            ActionButton(
+                label: "End Trip") {
+                print("End Trip")
+            }
+            
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(red: 253/255, green: 254/255, blue: 255/255))
+        .ignoresSafeArea()
     }
+}
+#Preview {
+    CurrentTripView()
 }
