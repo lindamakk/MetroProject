68df260dac17753751ee4e2c4b462adf6b6a8c38
Asaylhussin04 <asaylhussin77@gmail.com>
2026-02-13 23:30:06 +0300

    Destination Line Name

--- MetroProject/VIewModel/SharedData.swift
+++ MetroProject/VIewModel/SharedData.swift
@@ -21,24 +21,28 @@ struct MetroStationTEST :  Hashable {
     let name: String
     let longitude: Double
     let latitude: Double
+    let metroline: String
+
 }
 
 // Create 3 objects from your data
 let station1 = MetroStationTEST(
     name: "Dr Sulaiman Al Habib",
     longitude: 46.725799350815485,
-    latitude: 24.860932211685256
-
+    latitude: 24.860932211685256,
+    metroline: "Blue",
 )
 
 let station2 = MetroStationTEST(
     name: "KAFD",
     longitude: 46.72506156287345,
-    latitude: 24.860485030428944
+    latitude: 24.860485030428944,
+    metroline: "Blue",
 )
 
 let station3 = MetroStationTEST(
     name: "King Fahad District 2",
     longitude: 46.66338701990518,
-    latitude: 24.73665171554253
+    latitude: 24.73665171554253,
+    metroline: "Blue ",
 )
